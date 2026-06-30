7eabb53fc267d0e41d68ff05e6766e61f7b39c98
Anjaan Banjara <luckybanjara.anjaan@gmail.com>
2026-06-30 18:13:57 +0800

    feat(auth): started learning authentication

---
+++ server.js
@@ -0,0 +1,9 @@
+require ("dotenv").config();
+const app = require("./src/app.js")
+const connectDB =require("./src/db/db.js")
+
+connectDB();
+
+app.listen(3000, ()=>{
+    console.log("Server is running at port 3000!")
+})
